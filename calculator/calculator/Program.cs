fedc86fe3295c40bd10646d7d8399842eb0fcd89
“Varun13016” <dmevarun@gmail.com>
2021-10-31 13:03:16 +0530

    calc

---
+++ calculator/calculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sub obj = new Sub();
+            int result = obj.substract(10, 5);
+            Console.WriteLine("sub" + result);
+
+            int resultofsum = obj.addition(20, 30);
+            Console.WriteLine("sum" + resultofsum);
+
+            int resultofmult = obj.multiply(20, 30);
+            Console.WriteLine("multiply" + resultofmult);
+
+            int resultofdiv = obj.division(10, 3);
+            Console.WriteLine("division" + resultofdiv);
+        }
+    }
+}
---
+++ calculator/calculator/Sub.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace calculator
+{
+        public class Sub
+        {
+            int total;
+            public int substract(int num1, int num2)
+            {
+                return total = num1 - num2;
+            }
+            public int addition(int num1, int num2)
+            {
+                return total = num1 + num2;
+            }
+            public int multiply(int num1, int num2)
+            {
+                return total = num1 * num2;
+            }
+            public int division(int num1, int num2)
+            {
+                return total = num1 / num2;
+            }
+        }
+    }
